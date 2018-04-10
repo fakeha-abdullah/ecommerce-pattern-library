87a1c161c4b8612364938f245cbfa0e0d4956cd0
fakeha-abdullah <abdu0407@algonquinlive.com>
2018-04-10 10:39:32 -0400

    patternbot files

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/fakehaabdullah/Documents/Graphic Design Program/Semester 1/Semester 3/Semester 4/Web Dev IV/ecommerce-pattern-library
- * @version 1523334473261
+ * @version 1523368824567
  */
-const patternManifest_1523334473261 = {
+const patternManifest_1523368824567 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -932,5 +932,5 @@ const patternManifest_1523334473261 = {
   }
 };
 
-patternBotIncludes(patternManifest_1523334473261);
+patternBotIncludes(patternManifest_1523368824567);
 }());
